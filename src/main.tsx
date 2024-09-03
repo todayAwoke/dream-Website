04999657d1b15390bf068716a6a463c52c17f16c
todayAwoke <awokedejenie@gmail.com>
2024-09-03 10:40:57 +0300

    third

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react'; // Ensure React is imported
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ 
     <App />
-  </StrictMode>,
-)
+
+);
